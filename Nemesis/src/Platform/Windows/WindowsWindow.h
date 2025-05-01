1af715943c01244bbc9f8331110e45479d95acc9
Dominic Forster <forsterdominic2@gmail.com>
2025-05-01 23:34:32 +0200

    Add Input Polling

--- Nemesis/src/Platform/Windows/WindowsWindow.h
+++ Nemesis/src/Platform/Windows/WindowsWindow.h
@@ -18,6 +18,8 @@ namespace Nemesis {
 		void SetVSync(bool enabled) override;
 		bool IsVSync() const override;
 
+		inline virtual void* GetNativeWindow() const { return m_Window; }
+
 	private:
 		virtual void Init(const WindowProps& props);
 		virtual void Shutdown();
